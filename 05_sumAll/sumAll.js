c6ce45f665761681b24addafac29ddaa605f713b
mountuint <130184173+mountuint@users.noreply.github.com>
2024-07-03 11:42:36 +0300

    Solve some exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (firstNum, lastNum) {
+  if (!Number.isInteger(firstNum) || !Number.isInteger(lastNum)) {
+    return "ERROR";
+  }
 
+  if (firstNum < 0 || lastNum < 0) {
+    return "ERROR";
+  }
+
+  let startNum = firstNum;
+  let endNum = lastNum;
+
+  if (firstNum > lastNum) {
+    startNum = lastNum;
+    endNum = firstNum;
+  }
+
+  let total = 0;
+  for (let i = startNum; i <= endNum; i++) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let total = 1;
+  for (let i = 0; i < arr.length; i++) {
+    total *= arr[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0 || n === 1) return 1;
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +38,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
